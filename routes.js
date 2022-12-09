f3516c78a22e66224baed03c862517b94db1be9a
Mohammad Refaldo Rachman <mohrefaldorachman@gmail.com>
2022-12-10 01:59:51 +0700

    first commit

---
+++ routes.js
@@ -0,0 +1,45 @@
+const routes = [
+    {
+        method: 'GET',
+        path: '/hello/{name?}',
+        handler: (request, h) => {
+            const {name = "stranger"} = request.params;
+            const {lang} = request.query;
+
+            if(lang === 'id'){
+                return `Hai, ${name}!`;    
+            }
+            return `Hello, ${name}!`;
+        },
+    },
+    {
+        method: '*',
+        path: '/',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method tersebut';
+        },
+    },
+    {
+        method: 'GET',
+        path: '/about',
+        handler: (request, h) => {
+            return 'About page';
+        },
+    },
+    {
+        method: '*',
+        path: '/about',
+        handler: (request, h) => {
+            return 'Halaman tidak dapat diakses dengan method';
+        },
+    },
+    {
+        method: '*',
+        path: '/{any*}',
+        handler: (request, h) => {
+            return 'Halaman tidak ditemukan';
+        },
+    },
+];
+ 
+module.exports = routes;
